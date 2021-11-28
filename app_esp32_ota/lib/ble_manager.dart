feaf1e48a464966dfb65e50c5e3788760bf6686e
Raees Kattali <kattaliraees@gmail.com>
2021-11-28 13:34:49 +0300

    OTA

--- app_esp32_ota/lib/ble_manager.dart
+++ app_esp32_ota/lib/ble_manager.dart
@@ -1,124 +1,191 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
-class BLEManager extends ChangeNotifier {
-  final _otaServiceUUID = Uuid.parse("9A1B08AE-F898-08FF-329E-2E9AAF91F734");
-  final _otaCharacteristicsUUID =
-      Uuid.parse("2F6A8A4C-7DC7-4230-8668-5160AAA71DAA");
+typedef BLEConnect = void Function(bool);
 
-  int connectionState = 0;
+enum BLEConnectionStatus { Unknown, Connecting, Connected, FailedToConnect }
+
+class BLEProvider extends ChangeNotifier {
+
+  static const BLE_SERVICE_UUID = "6DD61F0E-9AEC-4C79-A62E-98910433CCC5";
+  static const BLE_CHAR_OTA_UUID = "6366C65E-37F4-4650-9AEF-5AEF56F67E1A";
+  static const GATT_SVR_SVC_ALERT_UUID = '1811';
+
+  //extends ChangeNotifier {
   final flutterReactiveBle = FlutterReactiveBle();
-  DiscoveredDevice? connectedBLEDevice;
-  String? foundDevciceID;
+  BLEConnectionStatus connectionStatus = BLEConnectionStatus.Unknown;
+  bool stopNotify = false;
+  String? foundDeviceID;
 
-  bool _isScanning = false;
-  int notifyCount = 0;
+  //OTA
+  double otaProgress = 0;
   bool isOTARunning = false;
 
-  Future<Uint8List> loadAsset() async {
-    final binBlob = await rootBundle.load('assets/sajdah_esp.bin');
-    Uint8List bytes = binBlob.buffer.asUint8List();
+  void scanAndConnect(Function cb, [String? deviceID]) {
+    StreamSubscription<BleStatus>? bleStatusStreamSubscription;
+    bleStatusStreamSubscription =
+        flutterReactiveBle.statusStream.listen((bleStatus) async {
+      if (bleStatus == BleStatus.ready) {
+        await bleStatusStreamSubscription!.cancel();
+        connectionStatus = BLEConnectionStatus.Connecting;
+        Stream<DiscoveredDevice> stream;
+        stream = flutterReactiveBle.scanForDevices(
+            withServices: [Uuid.parse(GATT_SVR_SVC_ALERT_UUID)],
+            scanMode: ScanMode.lowLatency);
 
-    return bytes;
+        StreamSubscription<DiscoveredDevice>? deviceStreamSubscription;
+        deviceStreamSubscription = stream.listen((d) async {
+          if (deviceID != null) {
+            if (d.id == deviceID) {
+              //Cancel searching if only the paired device is found
+              await deviceStreamSubscription!.cancel();
+              foundDeviceID = deviceID;
+            }
+          } else {
+            //Cancel searching once a new nearby SAJDAH device is founb
+            await deviceStreamSubscription!.cancel();
+          }
+
+          print(d.id.toLowerCase());
+
+          flutterReactiveBle
+              .connectToDevice(
+            id: d.id,
+            servicesWithCharacteristicsToDiscover: {
+              Uuid.parse(BLE_SERVICE_UUID): [
+                Uuid.parse(BLE_CHAR_OTA_UUID)
+              ]
+            },
+            connectionTimeout: const Duration(seconds: 2),
+          )
+              .listen((c) async {
+            if (c.connectionState == DeviceConnectionState.connected) {
+              connectionStatus = BLEConnectionStatus.Connected;
+              if (deviceID == null) {
+                foundDeviceID = d.id.toUpperCase();
+                if (!stopNotify) cb();
+              } else {
+                final characteristic = QualifiedCharacteristic(
+                    serviceId: Uuid.parse(BLE_SERVICE_UUID),
+                    characteristicId:
+                        Uuid.parse(BLE_CHAR_OTA_UUID),
+                    deviceId: d.id);
+                final response =
+                    await flutterReactiveBle.readCharacteristic(characteristic);
+                    final currentFirmwareVersion = utf8.decode(response);
+                print("currentFirmwareVersion - $currentFirmwareVersion");
+                if (response.length > 0) {
+                  if (!stopNotify) cb();
+                }
+              }
+            } else {
+              connectionStatus = BLEConnectionStatus.FailedToConnect;
+              if (!stopNotify) cb();
+            }
+
+            // Handle connection state updates
+          }, onError: (Object error) {
+            connectionStatus = BLEConnectionStatus.FailedToConnect;
+            // Handle a possible error
+          });
+        }, onError: (err) {});
+      }
+    });
   }
 
-  void writeBytesToOTACharecteristicsWithNotify() async {
-    if (connectionState == 1 && foundDevciceID!.isNotEmpty) {
-      if (isOTARunning) {
-        return;
-      }
+  Future<String> getFirmwareVersion() async {
+    if (connectionStatus == BLEConnectionStatus.Connected) {
+      final characteristic = QualifiedCharacteristic(
+          serviceId: Uuid.parse(BLE_SERVICE_UUID),
+          characteristicId: Uuid.parse(BLE_CHAR_OTA_UUID),
+          deviceId: foundDeviceID!);
+      final response =
+          await flutterReactiveBle.readCharacteristic(characteristic);
 
+      return utf8.decode(response);
+    }
+
+    return '';
+  }
+
+  void writeBytesToOTACharecteristicsWithNotify(Function cb) async {
+    if (isOTARunning) {
+      return;
+    }
+
+    if (connectionStatus == BLEConnectionStatus.Connected) {
       isOTARunning = true;
-      final otaData = await loadAsset();
-      final startKey = '(';
+      final otaData = await _getFirmwareBinBytes();
+      double totalSizeOfBin = otaData.length.toDouble();
+
+      final characteristic = QualifiedCharacteristic(
+          serviceId: Uuid.parse(BLE_SERVICE_UUID),
+          characteristicId: Uuid.parse(BLE_CHAR_OTA_UUID),
+          deviceId: foundDeviceID!);
+
+      print(DateTime.now().toString()); //OTA Start time
+      final otaDataSize = otaData.length;
+      List<int> sizeBytes = [
+        0x28,
+        (otaDataSize & 0xFF0000) >> 16,
+        (otaDataSize & 0x00FF00) >> 8,
+        (otaDataSize & 0x0000FF)
+      ];
       final endKey = ')';
-      final safetyDelay = 100;
       int start = 0;
       final chunkSize = 514;
       int totalRead = chunkSize;
       bool isLastChunkSent = false;
 
-      final characteristic = QualifiedCharacteristic(
-          serviceId: _otaServiceUUID,
-          characteristicId: _otaCharacteristicsUUID,
-          deviceId: foundDevciceID!);
-      flutterReactiveBle.writeCharacteristicWithoutResponse(characteristic,
-          value: startKey.codeUnits);
-      flutterReactiveBle.subscribeToCharacteristic(characteristic).listen(
-          (data) async {
+      StreamSubscription<List<int>>? bleStatusStreamSubscription;
+      bleStatusStreamSubscription = flutterReactiveBle
+          .subscribeToCharacteristic(characteristic)
+          .listen((event) async {
+        otaProgress = totalRead.toDouble() / totalSizeOfBin;
+        notifyListeners();
         if (isLastChunkSent) {
-          //await Future.delayed(Duration(milliseconds: safetyDelay));
-          flutterReactiveBle.writeCharacteristicWithoutResponse(characteristic,
+          await bleStatusStreamSubscription!.cancel();
+          await flutterReactiveBle.writeCharacteristicWithResponse(
+              characteristic,
               value: endKey.codeUnits);
           print('END OF OTA WRITE');
+          print(DateTime.now().toString());  //OTA End time
           isOTARunning = false;
+          otaProgress = 0;
+          notifyListeners();
+          cb();
+          return;
         }
-        notifyCount = notifyCount + 1;
-
-        if ((totalRead + chunkSize) > otaData.length) {
+        if (totalRead > otaData.length) {
           final lastChunk = otaData.getRange(start, otaData.length).toList();
-          await Future.delayed(Duration(milliseconds: safetyDelay));
           isLastChunkSent = true;
-          flutterReactiveBle.writeCharacteristicWithoutResponse(characteristic,
+          await flutterReactiveBle.writeCharacteristicWithResponse(
+              characteristic,
               value: lastChunk);
-          print(
-              '$notifyCount WRITING OTA BYTES FROM $start to ${otaData.length} to GATT SERVER');
+          print('OTA Progress - Sent last byte');
         } else {
           //await Future.delayed(Duration(milliseconds: safetyDelay));
           final chunk = otaData.getRange(start, totalRead).toList();
-          print(
-              '$notifyCount WRITING OTA BYTES FROM $start to $totalRead to GATT SERVER');
+          print('OTA Progress - $otaProgress');
           start = totalRead;
           totalRead = totalRead + chunkSize;
-          flutterReactiveBle.writeCharacteristicWithoutResponse(characteristic,
-              value: chunk);
+          await flutterReactiveBle
+              .writeCharacteristicWithResponse(characteristic, value: chunk);
         }
-
-        // code to handle incoming data
-      }, onError: (dynamic error) {
-        // code to handle errors
       });
+      await flutterReactiveBle.writeCharacteristicWithResponse(characteristic,
+          value: sizeBytes);
     }
   }
 
-  void scanAndConnect() {
-    if (_isScanning) {
-      //return;
-    }
-    _isScanning = true;
-    flutterReactiveBle.scanForDevices(
-        withServices: [], scanMode: ScanMode.lowLatency).listen((device) {
-      _isScanning = false;
-      //code for handling results
-      print('device ${device.name}');
-      if (device.name == 'SAJDAH') {
-        foundDevciceID = device.id;
-        connectionState = 1;
-        print(device.serviceUuids[0].toString());
-        flutterReactiveBle
-            .connectToDevice(
-          id: foundDevciceID!,
-          servicesWithCharacteristicsToDiscover: {
-            Uuid.parse(foundDevciceID!): [_otaCharacteristicsUUID]
-          },
-          connectionTimeout: const Duration(seconds: 2),
-        )
-            .listen((connectionState) {
-          if (connectionState.connectionState ==
-              DeviceConnectionState.connected) {
-            this.connectionState = 1;
-          }
-          // Handle connection state updates
-          print(connectionState);
-        }, onError: (Object error) {
-          // Handle a possible error
-        });
-      }
-    }, onError: (e) {
-      print('Scanning failed');
-    });
+  Future<Uint8List> _getFirmwareBinBytes() async {
+    final binBlob = await rootBundle.load('assets/ota.bin');
+    Uint8List bytes = binBlob.buffer.asUint8List();
+
+    return bytes;
   }
 }
--- app_esp32_ota/lib/main.dart
+++ app_esp32_ota/lib/main.dart
@@ -11,65 +11,42 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-        create: (_) => BLEManager(),
-        child: MaterialApp(
-          title: 'ESP32 BLE OTA Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: MyHomePage(title: 'ESP32 OTA'),
-        ));
+    return MaterialApp(
+      title: 'ESP32 BLE OTA Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'ESP32 OTA'),
+    );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  BLEManager? _ble;
-  @override
-  void initState() {
-    super.initState();
-  }
+  final ble = BLEProvider();
 
   @override
   Widget build(BuildContext context) {
-    _ble = Provider.of<BLEManager>(context, listen: true);
-    //print(_ble!.connectionState);
+    ble.scanAndConnect(() {});
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      body: Center(
+        child: MaterialButton(
+          child: Text('Start OTA'),
+          color: Colors.blue,
+          onPressed: () {
+            ble.writeBytesToOTACharecteristicsWithNotify(() async {
+              await Future.delayed(
+                  Duration(seconds: 2)); //Wait for ESP32 to restart
+              ble.scanAndConnect(() {});
+              final updatedFirmwareVersion = ble.getFirmwareVersion();
+              print(updatedFirmwareVersion);
+            });
+          },
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [buttonList()],
-          ),
-        ));
-  }
-
-  Widget buttonList() {
-    if (_ble!.connectionState == 1) {
-      return TextButton(
-          onPressed: () {
-            _ble!.writeBytesToOTACharecteristicsWithNotify();
-            //_ble!.writeBytesToOTACharecteristics();
-            //_ble!.test();
-          },
-          child: const Text('Start OTA'));
-    } else {
-      return TextButton(
-          onPressed: () {
-            _ble!.scanAndConnect();
-          },
-          child: const Text('Scan and Connect'));
-    }
+      ),
+    );
   }
 }
